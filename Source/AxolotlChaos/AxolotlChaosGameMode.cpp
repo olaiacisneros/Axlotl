8226fa486d58e86541baae8a0ab8910867b6e797
Andrés Alejandro Ragot <andresragot99@gmail.com>
2023-12-11 13:02:33 +0100

    11/12 mañana

--- Source/AxolotlChaos/AxolotlChaosGameMode.cpp
+++ Source/AxolotlChaos/AxolotlChaosGameMode.cpp
@@ -1,7 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "AxolotlChaosGameMode.h"
-#include "AxolotlChaosCharacter.h"
+#include "Axololt.h"
 #include "UObject/ConstructorHelpers.h"
 
 AAxolotlChaosGameMode::AAxolotlChaosGameMode()
