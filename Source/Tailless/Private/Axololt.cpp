4382f660cb472a3ceab2a8312821d1963e1372bd
Andrés Alejandro Ragot <andresragot99@gmail.com>
2024-03-11 22:56:29 +0100

    Reunion con Eneko animations

--- Source/Tailless/Private/Axololt.cpp
+++ Source/Tailless/Private/Axololt.cpp
@@ -383,7 +383,7 @@ void AAxololt::ApplyUpgrade(ENUM_UPGRADES _upgrade)
 		{
 			MaxHealth += 20;
 			Health = MaxHealth;
-			GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_More_life %d"), MaxHealth);
+			// GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_More_life %d"), MaxHealth);
 			UE_LOG(LogTemp, Display, TEXT("[Axololt.cpp]	Upgrade_More_life %d"), MaxHealth);
 			break;
 		}
@@ -392,7 +392,7 @@ void AAxololt::ApplyUpgrade(ENUM_UPGRADES _upgrade)
 		{
 			upgrade_room_life = true;
 			UE_LOG(LogTemp, Display, TEXT("[Axololt.cpp]	Upgrade_room_life true"));
-			GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_room_life true"));
+			// GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_room_life true"));
 			break;
 		}
 
@@ -400,7 +400,7 @@ void AAxololt::ApplyUpgrade(ENUM_UPGRADES _upgrade)
 		{
 			float addHealth = 10;
 			AddHealth(addHealth);
-			GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_Life_Add %i"), addHealth);
+			// GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_Life_Add %i"), addHealth);
 			UE_LOG(LogTemp, Display, TEXT("[Axololt.cpp]	Upgrade_Life_Add %i"), addHealth);
 			break;
 		}
@@ -409,7 +409,7 @@ void AAxololt::ApplyUpgrade(ENUM_UPGRADES _upgrade)
 		{
 			DashCounter = 2;
 			DashCounterAux = 2;
-			GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_Double_dash %i"), DashCounter);
+			// GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_Double_dash %i"), DashCounter);
 			UE_LOG(LogTemp, Display, TEXT("[Axololt.cpp]	Upgrade_Double_dash %i"), DashCounter);
 			break;
 		}
@@ -418,7 +418,7 @@ void AAxololt::ApplyUpgrade(ENUM_UPGRADES _upgrade)
 		{
 			float addAttack = 5;
 			BasicAttackDamage += addAttack;
-			GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_Basic_Attack %d"), addAttack);
+			// GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_Basic_Attack %d"), addAttack);
 			UE_LOG(LogTemp, Display, TEXT("[Axololt.cpp]	Upgrade_Basic_Attack %d"), addAttack);
 			break;
 		}
@@ -427,7 +427,7 @@ void AAxololt::ApplyUpgrade(ENUM_UPGRADES _upgrade)
 		{
 			float addAttack = 5;
 			BasicAttackComboDamage += addAttack;
-			GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_Basic_Combo %d"), addAttack);
+			// GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_Basic_Combo %d"), addAttack);
 			UE_LOG(LogTemp, Display, TEXT("[Axololt.cpp]	Upgrade_Basic_Combo %d"), addAttack);
 			break;
 		}
@@ -436,7 +436,7 @@ void AAxololt::ApplyUpgrade(ENUM_UPGRADES _upgrade)
 		{
 			float addAttack = 5;
 			SpecialAttackDamage += addAttack;
-			GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_Special_Attack %d"), addAttack);
+			// GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_Special_Attack %d"), addAttack);
 			UE_LOG(LogTemp, Display, TEXT("[Axololt.cpp]	Upgrade_Special_Attack %d"), addAttack);
 			break;
 		}
@@ -444,7 +444,7 @@ void AAxololt::ApplyUpgrade(ENUM_UPGRADES _upgrade)
 		case ENUM_UPGRADES::UPGRADE_COOLDOWN_SPECIAL:
 		{
 			CoolDownSpecialAttack = 1;
-			GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_Cooldown_Special 1"));
+			// GEngine->AddOnScreenDebugMessage(-1, 15, FColor::White, TEXT("[Axololt.cpp]	Upgrade_Cooldown_Special 1"));
 			UE_LOG(LogTemp, Display, TEXT("[Axololt.cpp]	Upgrade_Cooldown_Special 1"));
 			break;
 		}
